943ec7b32389d8ab0611e2d5d772a1afe353dda1
joao lucas camilo <jlf.camilo@icloud.com>
2021-07-11 17:20:44 -0400

    [notification] feat: finish email noitification, create rabbit singleton, create notification endpoint on controller.

--- controller/main.py
+++ controller/main.py
@@ -3,15 +3,45 @@ sys.path.append('../server')
 from flask import Flask
 from flask_restx import Api, Resource
 from instance import server
-from rabbit_server import channel
+from rabbit_server import rabbit_instance as rb
 import json
+
 app, api = server.app, server.api 
 
-@api.route('/test_notification')
+
+notification_queue = 'notification_queue'
+
+rb.create_queue(notification_queue)
+
+@api.route('/notification')
 class Notification(Resource):
-    def get(self,):
-        channel.basic_publish(exchange='', routing_key='notify_queue', body='algum teste aleatorio')
-        return "notification sent",200
+    @api.doc(responses={ 200: 'OK', 403: 'invalid json' },
+        params = {
+        'reservation_hash':'hash cretaed on reservation service',
+            "title":" title from reservation",
+            "spaces":"chosen spaces to be reserved",
+            "date_time":"date and time from reservation",
+            "email_list":"list of participants emails",
+            "obs":"some obs from reservations creator"
+    },)
+    
+    def post(self,):
+        body = api.payload
+        print(body)
+        if('reservation_hash' not in body):
+            response_json = json.dumps({
+                "message":"invalid json"
+            })
+            return response_json, 403
+        else:
+            to_publish_json = json.dumps(body)
+            response_json = json.dumps({
+                "message":"notification sent"
+            })
+
+            rb.publish(routing_key='notification_queue', body=to_publish_json)
+            return response_json,200
+    
 server.run()
 
 
