d9bf1273baea7f3566e832fe1bcde69b2b05c883
Rebeca <rebecacancelli@hotmail.com>
2021-07-17 21:55:41 -0400

    Created test.py equal to deleted booking.py

--- booking_service/models/test.py
+++ booking_service/models/test.py
@@ -1,27 +1,10 @@
-
-
 class Booking():
     def __init__(self, date, shift, email, lab_name):
-        
-        #Logica ticket
         self.ticket_id = 0
-        
         self.date = date
-
         self.shift = shift
-
-        self.email = email
-        
+        self.email= email
         self.lab_name = lab_name
-
-    def __repr__(self):
-        return "<Booking(ticket_id='%s', date='%s', shift='%s, email='%s lab_name='%s)>" % (
-            self.ticket_id,
-            self.date,
-            self.shift,
-            self.email,
-            self.lab_name
-        )
 
     def toDict(self):
         return {
@@ -31,3 +14,6 @@ class Booking():
             "email": self.email,
             "lab_name": self.lab_name
         }
+
+    def change_id(self, ticket_id):
+        self.ticket_id = ticket_id
